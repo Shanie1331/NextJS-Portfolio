867fd7449ff1ae3c13a87737aaf70007babf8215
Shashank Malviya <shashankm.malviya@gmail.com>
2023-07-24 19:18:47 +0530

    added pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      key: 'diversoIN.pem',
+      user : 'ubuntu',
+      host : '3.108.194.72',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Shanie1331/NextJS-Portfolio.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
